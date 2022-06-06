c49ca83573c59069a7aa0656b5b7e7490b6f4e68
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-06 09:13:04 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
- 
+
 function App() {
+const title = 'Welcome to React app';
+const num = 100;
+const link = 'https://www.google.com';
+//const people = {name: 'Barcode',age:10};
+
   return (
     <div className="App">
       <div className="content">
-        <h1>Welcome to react.js</h1>
+       <h1>{title}</h1>
+       <h2>{num}</h2>
+       <a href={link}>Google</a>
+       
+       <p>{100}</p>
+       <p>{"Hello"}</p>
+       <p>{[1,2,3,4,5]}</p>
+       <p>{Math.floor(Math.random()*5)}</p>
+
       </div>
     </div>
   );
 }
- 
+
 export default App;
