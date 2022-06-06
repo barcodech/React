b546fffcfee23b66c29c12e6a6089f7cbf3d64fe
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-06 09:25:18 +0700

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,26 @@
+import { useState,useEffect } from "react";
+import BlogList from "./BlogList";
+ 
+const Home = () => {
+ 
+    const [blogs,setblogs] = useState(null)
+ 
+    useEffect(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(res =>{
+          return res.json();
+        })
+        .then(data => {
+          setblogs(data);
+        })     
+    },[])
+ 
+    return (
+      <div className="home">
+        {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
+       
+      </div>
+    );
+  }
+   
+  export default Home;
